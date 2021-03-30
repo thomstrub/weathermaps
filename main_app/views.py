22e01e010eab45448006be5440238694c70a8d8e
Thomas Strub <thomstrub@gmail.com>
2021-03-30 16:23:22 -0700

    adds templating

--- main_app/views.py
+++ main_app/views.py
@@ -5,5 +5,5 @@ def home(request):
     return HttpResponse('<h1>Hello WeatherMaps<h1>')
 
 def about(request):
-    return HttpResponse('<h1>About WeatherMaps<h1>')
+    return render(request, 'about.html')
 

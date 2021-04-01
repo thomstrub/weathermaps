ab5273847b95351bbb2fb397d9b31dc0cb5d08c7
Thomas Strub <thomstrub@gmail.com>
2021-03-31 20:55:25 -0700

    adds Map model to models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Map(models.Model):
+    name: models.CharField(max_length=100)
+    region: models.CharField(max_length=100)
+    subregion: models.CharField(max_length=100)
+    elevation: models.IntegerField()
+    

2a707eac38e6b2918ba4111f523021f5841df9e1
Thomas Strub <thomstrub@gmail.com>
2021-03-31 20:10:52 -0700

    adds dummy data for views

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Map:
+    def __init__(self, name, region, subregion):
+        self.name = name
+        self.region = region
+        self.subregion = subregion
+        self.elevation = elevation
+
+maps = [
+    Map('Camp Muir', 'Paradise', 'Mt.Rainier', 8000),
+    Map('Mt. St. Helens South Ascent', 'Mt. St. Helens', 'South Cascades', 15000),
+    Map('Shipwreck Coast', 'Olympic Coast', 'Olympic Penninsula', 0)
+]
+
+
 def home(request):
     return HttpResponse('<h1>Hello WeatherMaps<h1>')
 

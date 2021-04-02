1dac6babfd48bdba5f91795ea0c1d08749b1a2f4
Thomas Strub <thomstrub@gmail.com>
2021-04-01 23:21:38 -0700

    replaces dummy data with model

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Map
 
-class Map:
-    def __init__(self, name, region, subregion, elevation):
-        self.name = name
-        self.region = region
-        self.subregion = subregion
-        self.elevation = elevation
+# class Map:
+#     def __init__(self, name, region, subregion, elevation):
+#         self.name = name
+#         self.region = region
+#         self.subregion = subregion
+#         self.elevation = elevation
 
-maps = [
-    Map('Camp Muir', 'Paradise', 'Mt.Rainier', 8000),
-    Map('Mt. St. Helens South Ascent', 'Mt. St. Helens', 'South Cascades', 15000),
-    Map('Shipwreck Coast', 'Olympic Coast', 'Olympic Penninsula', 0)
-]
+# maps = [
+#     Map('Camp Muir', 'Paradise', 'Mt.Rainier', 8000),
+#     Map('Mt. St. Helens South Ascent', 'Mt. St. Helens', 'South Cascades', 15000),
+#     Map('Shipwreck Coast', 'Olympic Coast', 'Olympic Penninsula', 0)
+# ]
 
 
 def home(request):
@@ -21,4 +22,5 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 def maps_index(request):
+    maps = Map.objects.all()
     return render(request, 'maps/index.html', {'maps': maps})

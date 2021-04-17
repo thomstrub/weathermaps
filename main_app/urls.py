591cc733966f30032090649bc0a56665d9dfdcbb
Thomas Strub <thomstrub@gmail.com>
2021-04-17 12:11:19 -0700

    updates urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('maps/', views.maps_index, name='index'),
     path('maps/<int:map_id>/', views.maps_detail, name='detail'),
     path('maps/create', views.MapCreate.as_view(), name='maps_create'),
+    path('maps/<int:pk>/update/', views.MapUpdate.as_view(), name='maps_update'),
+path('maps/<int:pk>/delete/', views.MapDelete.as_view(), name='maps_delete'),
 ]
